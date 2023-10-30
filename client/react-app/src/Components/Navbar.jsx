191d564753ae3a2a49b18c53f3f0a62aa37e6888
mesataksu <cmeaksu25@gmail.com>
2023-10-30 15:58:34 +0300

    Front-end base

---
+++ client/react-app/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+export function Navbar(){
+    return (
+        <div className = "navbar-container">
+            <div>
+                <h1>Logo</h1>
+            </div>
+            <div>
+                <button>Sign In</button>
+                <button>Sign Up</button>
+         
+            </div>
+        </div>
+    )
+}
