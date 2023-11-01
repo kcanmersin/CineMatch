8945ca674af27d1c33cd59834e2243a8aac35295
Erenayyilmaz <kenaneren67@gmail.com>
2023-11-01 11:19:57 +0300

    admin page tests and model change on movie

--- backend/APIuser/urls.py
+++ backend/APIuser/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserList, UserDetail
+from .views import UserList, UserDetail, UserView
 
 urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('users/<int:pk>/dnm/', UserView.as_view(), name='user-update'),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movie/', include('APImovie.urls')),
+    path('user/', include('APIuser.urls')),
 ]
