bcfbf3e63baba35877948fc207048e52acddd893
Erenayyilmaz <kenaneren67@gmail.com>
2023-11-20 20:16:38 +0300

    list structure has been initialized. Upvote and downvote have bug it will be fixed. added in admin page and list has been tested

--- backend/APImovie/admin.py
+++ backend/APImovie/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import Movie
+from .models import Movie, MovieList, Collection, Genre, ProductionCompany, ProductionCountry, Language
 
 class MovieResource(resources.ModelResource):
     class Meta:
@@ -11,3 +11,9 @@ class MovieAdmin(ImportExportModelAdmin):
     resource_class = MovieResource
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(MovieList)
+admin.site.register(Collection)
+admin.site.register(Genre)
+admin.site.register(ProductionCompany)
+admin.site.register(ProductionCountry)
+admin.site.register(Language)
