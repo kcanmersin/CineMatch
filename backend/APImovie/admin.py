378a7465a0c4493de1ae432990f302e1f3c11f1c
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2023-11-22 22:11:44 +0300

    configure admin site

--- backend/APImovie/admin.py
+++ backend/APImovie/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
+from .models import Movie, Genre, Keyword, MovieKeyword,Credit
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import Movie, MovieList, Collection, Genre, ProductionCompany, ProductionCountry, Language
-
 class MovieResource(resources.ModelResource):
     class Meta:
         model = Movie
@@ -10,10 +9,30 @@ class MovieResource(resources.ModelResource):
 class MovieAdmin(ImportExportModelAdmin):
     resource_class = MovieResource
 
+class GenreResource(resources.ModelResource):
+    class Meta:
+        model = Genre
+
+class GenreAdmin(ImportExportModelAdmin):
+    resource_class = GenreResource
+
+class KeywordResource(resources.ModelResource):
+    class Meta:
+        model = Keyword
+
+class KeywordAdmin(ImportExportModelAdmin):
+    resource_class = KeywordResource
+
+class CreditResource(resources.ModelResource):
+    class Meta:
+        model = Credit
+
+class CreditAdmin(ImportExportModelAdmin):
+    resource_class = CreditResource
+
+
+
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(MovieList)
-admin.site.register(Collection)
-admin.site.register(Genre)
-admin.site.register(ProductionCompany)
-admin.site.register(ProductionCountry)
-admin.site.register(Language)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Keyword, KeywordAdmin)
+admin.site.register(Credit, CreditAdmin)
