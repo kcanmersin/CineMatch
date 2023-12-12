bb15d27619e89308eb086f0a8b1256d9e96081bc
Erenayyilmaz <kenaneren67@gmail.com>
2023-12-12 14:21:30 +0300

    conflict solve

--- backend/APImovie/models.py
+++ backend/APImovie/models.py
@@ -1,67 +1,127 @@
 from django.db import models
+from accounts.models import UserAccount
+from django.urls import reverse
+from django.utils.text import slugify
 
-class Collection(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    poster_path = models.CharField(max_length=255)
-    backdrop_path = models.CharField(max_length=255, null=True, blank=True)
+class MovieList(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, unique=True, blank=False, null= False)
+#    title = models.CharField(max_length=255, default='Untitled')
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='lists', null=True, blank=True)
+    is_public = models.BooleanField(default=True, null=True)
+    movies = models.ManyToManyField('Movie', related_name='lists', blank=True)
+    upvotes = models.PositiveIntegerField(default=0, null=True, blank=True)
+    downvotes = models.PositiveIntegerField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    total_time_of_movies = models.PositiveIntegerField(default=0, null=True, blank=True)
 
+    class Meta:
+        ordering = ['-upvotes']  # Optional: Order lists by upvotes in descending order
+
+    def get_upvotes(self):
+        return self.votes.filter(is_upvote=True).count()
+    
+    def get_downvotes(self):
+        return self.votes.filter(is_upvote=False).count()
+    
     def __str__(self):
-        return self.name
+        return self.title
 
-class Genre(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
+class Vote(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    movie_list = models.ForeignKey(MovieList, on_delete=models.CASCADE, related_name='votes')
+    is_upvote = models.BooleanField()
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        unique_together = ('user', 'movie_list')
 
-class ProductionCompany(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-class ProductionCountry(models.Model):
-    iso_3166_1 = models.CharField(max_length=2, primary_key=True)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-class Language(models.Model):
-    iso_639_1 = models.CharField(max_length=2, primary_key=True)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
 
 class Movie(models.Model):
-    adult = models.BooleanField(default=False)
-    belongs_to_collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
-    budget = models.IntegerField(default=0, null=True)  # Nullable budget field
-    genres = models.ManyToManyField(Genre)
-    homepage = models.URLField(null=True, blank=True)
-    id = models.IntegerField(primary_key=True)
-    imdb_id = models.CharField(max_length=255, default='', null=True, blank=True)  # Nullable IMDb ID field
-    original_language = models.CharField(max_length=2,null= True)
-    original_title = models.CharField(max_length=255,null= True)
-    overview = models.TextField(null= True)
-    popularity = models.FloatField(default=0.0, null=True)  # Nullable popularity field
-    poster_path = models.CharField(max_length=255,null= True)
-    production_companies = models.ManyToManyField(ProductionCompany)
-    production_countries = models.ManyToManyField(ProductionCountry)
-    release_date = models.DateField(null=True)  # Nullable release_date field
-    revenue = models.FloatField(default=0.0, null=True)  # Nullable revenue field
-    runtime = models.FloatField(default=0.0, null=True)  # Nullable runtime field
-    spoken_languages = models.ManyToManyField(Language)
-    status = models.CharField(max_length=255, null=True, blank=True)  # Nullable status field
-    tagline = models.TextField(null=True, blank=True)
+    tmdb_id = models.IntegerField(primary_key=True)
+    imdb_id = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    video = models.BooleanField(default=False, null=True)  # Nullable video field
-    vote_average = models.FloatField(default=0.0, null=True)  # Nullable vote_average field
-    vote_count = models.IntegerField(default=0, null=True)  # Nullable vote_count field
+    poster_path = models.CharField(max_length=255)
+    background_path = models.CharField(max_length=255, null=True, blank=True)
+    original_language = models.CharField(max_length=20)
+    original_title = models.CharField(max_length=255)
+    overview = models.TextField()
+    release_date = models.CharField(max_length=255)
+    runtime = models.IntegerField()  # Updated to DecimalField
+    vote_average = models.CharField(max_length=255)  # Updated to DecimalField
+    vote_count = models.IntegerField()
+    popularity = models.CharField(max_length=255)  # Updated to DecimalField
 
     def __str__(self):
         return self.title
+class Genre(models.Model):
+    id = models.IntegerField(primary_key=True)
+    genre_name = models.CharField(max_length=50, default = 'Default', blank=False, null= False)
+
+    def __str__(self):
+        return self.genre_name
+class Movie_Genre(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,null = True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE,null = True)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.genre.genre_name}"
+    
+
+class Actor(models.Model):
+    id =  models.IntegerField(primary_key=True)
+    actor_name = models.CharField(max_length=255)
+    profile_path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.actor_name
+
+class Character(models.Model):
+    id = models.IntegerField(primary_key=True)
+    character_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.character_name
+
+class Cast(models.Model):
+    id = models.IntegerField(primary_key=True)
+    actor_id = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.actor_id.actor_name} - {self.character_id.character_name} - {self.movie_id.title}"
+
+class Crew(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name + " - " + self.role
+
+class MovieCrew(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,null = True)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE,null = True)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.crew.name} - {self.crew.role}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:50]}"
+    
+
+class Rate(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='rates')
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    rate_point = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} rates {self.movie.title} with {self.rate_point} points"
--- backend/APImovie/serializers.py
+++ backend/APImovie/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, MovieList, Vote, Genre, Comment
+from django.urls import reverse
+
+class CommentSerializer(serializers.ModelSerializer):
+    movie = serializers.StringRelatedField(read_only=True) # user yorum yaparken başka bir user olarak ve ya başka bir filme yorum yapmasın diye
+    #user = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    comments = serializers.SerializerMethodField()
+    genres = GenreSerializer(many=True, read_only=True)
 
     class Meta:
         model = Movie
@@ -16,5 +32,78 @@ class MovieSerializer(serializers.ModelSerializer):
     def validate(self, data):
         return super().validate(data)
     
+    def get_comments(self,obj):
+        comments = Comment.objects.filter(movie=obj)[:3]
+        request = self.context.get('request')
+        return{
+            "comments" : CommentSerializer(comments,many=True).data,
+            "all_comment_link":request.build_absolute_uri(reverse('movie_comment_list',kwargs={'movie_id':obj.id}))
+    }
     
+
+class MovieListAddSerializer(serializers.ModelSerializer):
+    movie_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = MovieList
+        fields = ('id', 'title', 'movie_id')
+        read_only_fields = ('id', 'title')
+
+    def validate(self, data):
+        movie_id = data.get('movie_id')
+        if not Movie.objects.filter(id=movie_id).exists():
+            raise serializers.ValidationError("Movie does not exist.")
+        return data
+
+    def create(self, validated_data):
+        movie_id = validated_data.pop('movie_id')
+        movie = Movie.objects.get(id=movie_id)
+        instance = super().create(validated_data)
+        instance.movies.add(movie)
+        return instance
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('user', 'movie_list', 'is_upvote')
+
+    def create(self, validated_data):
+        vote, created = Vote.objects.update_or_create(
+            user=validated_data.get('user', None),
+            movie_list=validated_data.get('movie_list', None),
+            defaults={'is_upvote': validated_data.get('is_upvote', False)}
+        )
+        return vote
+
+class MovieListSerializer(serializers.ModelSerializer):
+    upvotes = serializers.IntegerField(source='votes.filter(is_upvote=True).count', read_only=True)
+    downvotes = serializers.IntegerField(source='votes.filter(is_upvote=False).count', read_only=True) # Burayı kontrol et
+    movies = MovieSerializer(many=True, read_only=True)
+    votes = VoteSerializer(many=True, read_only=True)  # Include votes in the response
+    total_time_of_movies = serializers.IntegerField(read_only=True)
+    number_of_movies = serializers.SerializerMethodField()
+
+
+    class Meta:
+        model = MovieList
+        fields = ('id', 'title', 'user', 'is_public', 'movies', 'upvotes', 'downvotes', 'votes', 'total_time_of_movies', 'number_of_movies') # votes eklendi
+
     
+    def get_number_of_movies(self, obj):
+        return obj.movies.count()
+
+    def get_upvotes(self, obj):
+        return obj.get_upvotes()
+
+    def get_downvotes(self, obj):
+        return obj.get_downvotes()
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        movies_data = validated_data.pop('movies', [])  # remove movies from validated_data
+        validated_data.pop('user', None)  # remove user from validated_data
+        movie_list = MovieList.objects.create(user=user, **validated_data)
+        for movie_data in movies_data:
+            movie = Movie.objects.create(**movie_data)
+            movie_list.movies.add(movie)
+        return movie_list
