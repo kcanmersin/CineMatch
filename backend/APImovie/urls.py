b5f760718bd37a588969d844a28847261dfc9713
Erenayyilmaz <74668808+Erenayyilmaz@users.noreply.github.com>
2023-12-12 16:13:51 +0300

    urlsapimovieconf

--- backend/APImovie/urls.py
+++ backend/APImovie/urls.py
@@ -15,7 +15,7 @@
 # ]
 
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, VoteView, MovieListCreateView, MovieListDetailView, MovieListViewSet, MovieListRetrieveAddView, GenreMovieListView
+from .views import MovieViewSet, VoteView, MovieListCreateView, MovieListDetailView, MovieListViewSet, MovieListRetrieveAddView, GenreMovieListView, UsersMovieListView, MovieCommentListCreateView, MovieCommentDetailView
 from django.urls import path, include
 
 movie_router = DefaultRouter()
@@ -29,6 +29,9 @@ urlpatterns = [
     path('lists/', MovieListViewSet.as_view({'get': 'list'}), name='movie-list-list'),
     path('lists/<int:pk>/', MovieListDetailView.as_view(), name='movie-list-detail'),
     path('lists/create/', MovieListCreateView.as_view(), name='movie-list-create'),
+    path('<int:user_id>/lists/', UsersMovieListView.as_view(), name='users-movie-list'),
+    path('comment_list/<int:movie_id>/', MovieCommentListCreateView.as_view(), name='movie_comment_list'),
+    path('comment_detail/movie/<int:movie_id>/comment/<int:comment_id>/', MovieCommentDetailView.as_view(), name='movie_comment_detail'),
     path('votes/', VoteView.as_view({'post': 'create'}), name='vote-create'),
     path('movie-lists/', MovieListRetrieveAddView.as_view(), name='movie-list-retrieve-add'),
     path('genres/<slug:genre_slug>/', GenreMovieListView.as_view(), name='genre-movie-list'),
