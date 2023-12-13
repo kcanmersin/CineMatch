e6aa8931698508e8a8d7772519d9d5ef80a36a55
diyarrr <isidiyar1071@gmail.com>
2023-12-13 16:19:50 +0300

    user page is nearly done

--- client/react-app/src/Components/MyProfilePage.jsx
+++ client/react-app/src/Components/MyProfilePage.jsx
@@ -1,4 +1,5 @@
 import "./MyProfilePage.css"
+import { useState, useEffect } from "react";
 import { Link} from "react-router-dom";
 import ProgramNavbar from "./SubComponents/ProgramNavbar";
 import Button from "react-bootstrap/esm/Button";
@@ -8,7 +9,15 @@ import Row from "react-bootstrap/Row";
 
 
 export default function MyProfilePage(){
+    const [profileData, setProfileData] = useState({
+        username: "Loading...",
+        matchRate: 0,
+        followerCount: 0,
+        followingCount: 0,
+        profilePictureUrl: ''
+    });
 
+    const jwtAccess = localStorage.getItem('jwtAccess');
 
     /*  ---------------------------------------------------------------------------
         --------------------------------------------------------------------------
@@ -18,14 +27,62 @@ export default function MyProfilePage(){
         backendde bi defa çekip bi yerde tutup oradan çekebiliyosan öyle yap
     */
 
+    useEffect(() => {
+        // Fetch username data
+        fetch('http://127.0.0.1:8000/auth/users/me', {
+            method: 'GET',
+            headers: {
+                'Authorization': `JWT ${jwtAccess}`,
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            // Set username in profile data
+            const fetchedUsername = data.username;
+            setProfileData(prevData => ({ ...prevData, username: fetchedUsername }));
+
+            // Fetch profile data using the fetched username
+            return fetch(`http://127.0.0.1:8000/accounts/profile/${fetchedUsername}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `JWT ${jwtAccess}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+        })
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(profileInfo => {
+            // Update state with profile information
+            setProfileData(prevData => ({
+                ...prevData,
+                matchRate: profileInfo.match_rate,
+                followerCount: profileInfo.follower_count,
+                followingCount: profileInfo.following_count,
+                profilePictureUrl: profileInfo.profile_picture_url
+            }));
+        })
+        .catch(error => console.error('There has been a problem with your fetch operations:', error));
+    }, [jwtAccess]);
+    
     const MyProfileBgImage= "src/assets/dummy1.jpg";
     const ppLink= "src/assets/pp.jpg";
-    const username="Michael Corleone";
+    //const username="Michael Corleone";
 
     
     const movieCount="1071";
-    const followersCount= "1453";
-    const followingsCount= "1923"
+    //const followersCount= "1453";
+    //const followingsCount= "1923"
 
     // Dummy movie data
     const movieData = [
@@ -58,17 +115,19 @@ export default function MyProfilePage(){
                         className="user-profile-image"
                         src={ppLink}
                     />
-                    <div className="user-name">{username}</div>
+                    <div className="user-name">{profileData.username}</div>
                 </div>
                 <div className="follow-stats">
                     <div><span style={{fontWeight: 'bold'}}>{movieCount}</span> MOVIES</div>
-                    <div><span style={{fontWeight: 'bold'}}>{followersCount}</span> FOLLOWERS</div>
-                    <div><span style={{fontWeight: 'bold'}}>{followingsCount}</span> FOLLOWINGS</div>
+                    <div><span style={{fontWeight: 'bold'}}>{profileData.followerCount}</span> FOLLOWERS</div>
+                    <div><span style={{fontWeight: 'bold'}}>{profileData.followingCount}</span> FOLLOWINGS</div>
                 </div>
             </div>
             <div className="rest-myprofile-page">
                 <div className="profile-buttons-container">
-                    <Button variant="success profile-button">LISTS</Button>
+                    <Link to="/mylists">
+                        <Button variant="success profile-button">LISTS</Button>
+                    </Link>
                     <Button variant="success profile-button">STATS</Button>
                 </div>
                 <div className= "watched-movies">
--- client/react-app/src/Components/SubComponents/ProgramNavbar.jsx
+++ client/react-app/src/Components/SubComponents/ProgramNavbar.jsx
@@ -12,10 +12,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { AuthContext } from "../../auth/AuthContext";
 import  { useContext } from 'react';
+import { useEffect } from "react";
 
 
 export default function ProgramNavbar() {
+  const [username, setUsername] = useState("Loading...");
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const jwtAccess = localStorage.getItem('jwtAccess');
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -28,6 +31,27 @@ export default function ProgramNavbar() {
   const handleLogout = () => {
       logout();
   };
+
+  // retrieve the username
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/auth/users/me', {
+        method: 'GET',
+        headers: {
+            'Authorization': `JWT ${jwtAccess}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+        setUsername(data.username); // Set the fetched username
+    })
+    .catch(error => console.error('There has been a problem with your fetch operation:', error));
+}, [jwtAccess]);
 
   const UserName= "Michael Corleone";
   const ppLink= "src/assets/pp.jpg";
@@ -54,7 +78,7 @@ export default function ProgramNavbar() {
           />
         </Nav.Link>
         <Nav.Link as={Link} to="/myprofile" className="mx-1 navbar-username">
-          <div style={{ color: '#CECECE' }}>{UserName}</div>
+          <div style={{ color: '#CECECE' }}>{username}</div>
         </Nav.Link>
         <button  className="side-menu-button mx-3" type= "button" aria-label="Toogle Navigation"
         onClick={() => toggleSidebar()}> 
