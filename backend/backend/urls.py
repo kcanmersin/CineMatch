2c66ae5eed7f49a5a57f4dd081876dde7a43a774
Kerim Can Mersin <73068663+kcanmersin@users.noreply.github.com>
2023-12-14 01:38:00 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('accounts/media/', include(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))),
 
 ]
-
+#for deploy
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
