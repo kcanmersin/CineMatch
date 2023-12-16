b245deed9f6361386ff812a4dcfa63e5ee54a6c8
Erenayyilmaz <74668808+Erenayyilmaz@users.noreply.github.com>
2023-12-16 20:12:56 +0300

    dummy AI for front

--- backend/AI/movietomovie.py
+++ backend/AI/movietomovie.py
@@ -7,10 +7,11 @@ from AI.NCFmodel import NCF  # Import the NCF class from the AI module
 from pathlib import Path
 
 def movie_to_movie(movieId):
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    # BASE_DIR = Path(__file__).resolve().parent.parent
 
-    with open(str(BASE_DIR) + "\AI\movie_recommendation_model.pkl", "rb") as pkl_file:
-        loaded_model = joblib.load(pkl_file)
+    # with open(str(BASE_DIR) + "\AI\movie_recommendation_model.pkl", "rb") as pkl_file:
+    #     loaded_model = joblib.load(pkl_file)
 
-    similar_movies = loaded_model.recommend_similar_movies(movieId)
-    return similar_movies
+    # similar_movies = loaded_model.recommend_similar_movies(movieId)
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # return similar_movies
