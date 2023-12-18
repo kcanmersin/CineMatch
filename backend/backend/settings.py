ca59bee4fab9232241920fb4982fbee3f8b78cc2
Erenayyilmaz <74668808+Erenayyilmaz@users.noreply.github.com>
2023-12-18 18:35:51 +0300

    new database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db2.sqlite3',
+        'NAME': BASE_DIR / 'db5.sqlite3',
         'OPTIONS': {
             'timeout': 20,  # Set the timeout value in seconds
         },
